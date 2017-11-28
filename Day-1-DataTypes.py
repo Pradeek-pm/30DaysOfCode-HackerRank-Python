fcf0e4dc5463132fce1080cf2c6426003fc3a9aa
Pradeek-pm <34047298+Pradeek-pm@users.noreply.github.com>
2017-11-28 07:06:19 +0530

    Create Day-1-DataTypes.py

---
+++ Day-1-DataTypes.py
@@ -0,0 +1,12 @@
+# Declare second integer, double, and String variables.
+ii = int(input())
+dd = float(input())
+ss = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + ii)
+
+# Print the sum of the double variables on a new line.
+print(d + dd)
+
+print(s + ss)
