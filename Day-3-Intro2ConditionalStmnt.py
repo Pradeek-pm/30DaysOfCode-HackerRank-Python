186d75f1a4da0ec17d9fb41e6d3f584070e00a85
Pradeek-pm <34047298+Pradeek-pm@users.noreply.github.com>
2017-11-30 07:29:17 +0530

    Create Day-3-Intro2ConditionalStmnt.py

---
+++ Day-3-Intro2ConditionalStmnt.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+import sys
+
+
+N = int(input().strip())
+if ((N % 2 != 0) or N % 2== 0 and 6<=N<=20) :
+    print("Weird")
+elif (N >20 or N==2 or N==4 ) :
+    print("Not Weird")  
